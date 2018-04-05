aa7515ef145802bb8b7b8687b4e162b3c7026a5e
Alireza <alireza@Alirezas-MacBook-Pro.local>
2018-04-05 12:58:13 +0200

    Add project structure

---
+++ src/main/java/at/refugeescode/mp10phonebookpersonandaddressonetoone/Mp10PhoneBookPersonAndAddressOneToOneApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp10phonebookpersonandaddressonetoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp10PhoneBookPersonAndAddressOneToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp10PhoneBookPersonAndAddressOneToOneApplication.class, args);
+	}
+}
